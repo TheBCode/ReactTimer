6bb38b87bb419733fc799b82698fc22692b320c1
Byron Buyck <bbuyck1@gmail.com>
2017-11-18 05:00:28 -0500

    Add static controls component

--- karma.conf.js
+++ karma.conf.js
@@ -5,9 +5,9 @@ module.exports = function (config) {
        browsers: ['Chrome'],
        singleRun: true,
        frameworks: ['mocha'],
-       files: ['app/tests/**/*.tests.jsx'],
+       files: ['app/tests/**/*.test.jsx'],
        preprocessors: {
-           'app/tests/**/*.tests.jsx': ['webpack', 'sourcemap']
+           'app/tests/**/*.test.jsx': ['webpack', 'sourcemap']
        },
        reporters: ['mocha'],
        client: {
